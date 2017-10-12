f654ba7c209f24c98236f66565a988536204d44b
Nathan Steinbaugh <ste19@live.missouristate.edu>
2017-10-12 03:06:12 -0500

    Add bootstrap layout to the site and add cshtml pages

--- WillardPropertiesLLC/App_Start/BundleConfig.cs
+++ WillardPropertiesLLC/App_Start/BundleConfig.cs
@@ -12,7 +12,7 @@ namespace WillardPropertiesLLC
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jqBootstrapValidation.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -21,11 +21,14 @@ namespace WillardPropertiesLLC
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/popper.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap-grid.css",
+                      "~/Content/bootstrap-reboot.css",
+                      "~/Content/modern-business.css"));
         }
     }
 }
